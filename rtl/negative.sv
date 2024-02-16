c351b03f9c55fb852abbca7e3ec3d83c18378bb9
Iztok Jeras <iztok.jeras@cosylab.com>
2024-02-16 18:18:06 +0100

    added negative RTL

--- rtl/negative.sv
+++ rtl/negative.sv
@@ -1,4 +1,4 @@
-module onehot_rightmost_alu #(
+module negative #(
     int unsigned XLEN = 32
 )(
     // system signals
@@ -11,24 +11,15 @@ module onehot_rightmost_alu #(
 
     // input register
     logic [XLEN-1:0] xr;
-(* keep = "true" *)    logic [XLEN-1:0] xr_neg;  // negative
 
     // use synchronous reset
     always @(posedge clk)
     if (rst)  xr <= '0;
     else      xr <= xi;
 
-    assign xr_neg = -xr;
-
-    // function definition
-    function logic [XLEN-1:0] onehot_rightmost_f (logic [XLEN-1:0] x);
-        onehot_rightmost_f = x & (-x);
-    endfunction: onehot_rightmost_f;
-
     // use synchronous reset
     always @(posedge clk)
     if (rst)  xo <= '0;
-//    else      xo <= onehot_rightmost_f(xr);
-    else      xo <= xr & xr_neg;
+    else      xo <= -xr;
 
-endmodule: onehot_rightmost_alu
+endmodule: negative

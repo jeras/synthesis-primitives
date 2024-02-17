0f526f6d9f4f42746aac5ae1e187b7dfd541c75b
Iztok Jeras <iztok.jeras@gmail.com>
2024-02-17 20:18:53 +0100

    added onehot_logarithm

--- rtl/onehot_rightmost_alu.sv
+++ rtl/onehot_rightmost_alu.sv
@@ -5,13 +5,12 @@ module onehot_rightmost_alu #(
     input  logic clk,
     input  logic rst,
     // data signals
-(* IOB = "FALSE" *)    input  logic [WIDTH-1:0] xi,
-(* IOB = "FALSE" *)    output logic [WIDTH-1:0] xo
+    input  logic [WIDTH-1:0] xi,
+    output logic [WIDTH-1:0] xo
 );
 
     // input register
     logic [WIDTH-1:0] xr;
-//(* keep = "true" *)    logic [WIDTH-1:0] xr_neg;  // negative
     logic [WIDTH-1:0] xr_neg;  // negative
 
     // use synchronous reset
@@ -21,15 +20,9 @@ module onehot_rightmost_alu #(
 
     assign xr_neg = -xr;
 
-    // function definition
-    function logic [WIDTH-1:0] onehot_rightmost_f (logic [WIDTH-1:0] x);
-        onehot_rightmost_f = x & (-x);
-    endfunction: onehot_rightmost_f;
-
     // use synchronous reset
     always @(posedge clk)
     if (rst)  xo <= '0;
-//    else      xo <= onehot_rightmost_f(xr);
     else      xo <= xr & xr_neg;
 
 endmodule: onehot_rightmost_alu

4f17bb0519f9937c999dd99591a6d9b0fdeba5d8
Iztok Jeras <iztok.jeras@gmail.com>
2024-02-26 15:02:15 +0100

    added priority_encoder to synthesis

--- rtl/synthesis_optimization_top.sv
+++ rtl/synthesis_optimization_top.sv
@@ -30,8 +30,8 @@ module synthesis_optimization_top #(
     logic [NUMBER-1:0] [WIDTH-1:0] par_o;
     logic [NUMBER-1:0]             ser_o;
 
-    assign ser_i = p_i;
-    assign p_o = ser_o;
+    assign ser_i = p_i[NUMBER-1:0];
+    assign p_o[NUMBER-1:0] = ser_o;
 
     assign gpo = '0;
 
@@ -101,6 +101,67 @@ module synthesis_optimization_top #(
         // data signals
         .xi    (par_o[3]),
         .idx   (par_i[3][$clog2(WIDTH)-1:0])
+    );
+
+////////////////////////////////////////////////////////////////////////////////
+// encoder I/O placeholders
+////////////////////////////////////////////////////////////////////////////////
+
+    localparam int unsigned NUMBER_ENC = 1;
+
+    logic [NUMBER_SER-1:0]                     dec_ser_i;
+    logic [NUMBER_SER-1:0] [       WIDTH -1:0] dec_par_i;
+    logic [NUMBER_SER-1:0] [       WIDTH -1:0] dec_par_o;
+    logic [NUMBER_SER-1:0]                     dec_ser_o;
+
+    logic [NUMBER_SER-1:0] [$clog2(WIDTH)-0:0] enc_par_i;
+    logic [NUMBER_SER-1:0]                     enc_ser_o;
+
+    assign dec_ser_i = p_i[NUMBER_ENC+NUMBER-1:NUMBER];
+    assign p_o[NUMBER_ENC+NUMBER-1:NUMBER] = dec_ser_o ^ enc_ser_o ;
+
+    placeholder #(
+        .WIDTH  (WIDTH)
+    ) placeholder_dec [0:NUMBER_ENC-1] (
+        // system signals
+        .clk    (clk),
+        .rst    (rst),
+        // control inputs
+        .pld    (gpi[0]),
+        // data inputs
+        .ser_i  (dec_ser_i),
+        .par_i  (dec_par_i),
+        // data outputs
+        .par_o  (dec_par_o),
+        .ser_o  (dec_ser_o)
+    );
+
+    placeholder #(
+        .WIDTH  (WIDTH)
+    ) placeholder_enc [0:NUMBER_ENC-1] (
+        // system signals
+        .clk    (clk),
+        .rst    (rst),
+        // control inputs
+        .pld    (gpi[0]),
+        // data inputs
+        .ser_i  (enc_ser_i),
+        .par_i  (enc_par_i),
+        // data outputs
+        .par_o  (enc_par_o),
+        .ser_o  (enc_ser_o)
+    );
+
+////////////////////////////////////////////////////////////////////////////////
+// encoder RTL
+////////////////////////////////////////////////////////////////////////////////
+
+    priority_encoder #(
+        .WIDTH (WIDTH)
+    ) priority_encoder (
+        .dec_vld (dec_par_o),
+        .enc_idx (enc_par_i[$clog2(WIDTH)-1:0]),
+        .enc_vld (enc_par_i[$clog2(WIDTH)    ])
     );
 
 endmodule: synthesis_optimization_top

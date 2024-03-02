b24c4a76d85fc9325a1f1942f6c1569a1b8cc627
Iztok Jeras <iztok.jeras@gmail.com>
2024-03-02 17:18:24 +0100

    cleaning up various implementation options

--- rtl/priority_encoder.sv
+++ rtl/priority_encoder.sv
@@ -6,9 +6,15 @@ module priority_encoder #(
     localparam int unsigned WIDTH_LOG = $clog2(WIDTH),
     localparam int unsigned SPLIT_LOG = $clog2(SPLIT),
     // implementation
-    parameter  int unsigned MODE = 0  // supported modes are 0-LUT, 1-ADDER and 2-FUNCTION
+    // 0 - casez
+    // 1 - unique   if
+    // 2 - priority if
+    // 3 - unique   case inside
+    // 4 - priority case inside
+    parameter  int unsigned IMPLEMENTATION = 4
 )(
-    input  logic [SPLIT-1:0][WIDTH/SPLIT-1:0] dec_vld,
+//    input  logic [SPLIT-1:0][WIDTH/SPLIT-1:0] dec_vld,
+    input  logic            [WIDTH      -1:0] dec_vld,
     output logic            [WIDTH_LOG  -1:0] enc_idx,
     output logic                              enc_vld
 );
@@ -16,76 +22,57 @@ module priority_encoder #(
     generate
     if (WIDTH == SPLIT) begin: leaf
 
-        case (MODE)
-
-            0:  // LUT
-            always_comb
-            begin
-                case (SPLIT)
-                    2:
-                    unique case (dec_vld) inside
-                        2'b?1  : enc_idx = 1'd0;
-                        2'b10  : enc_idx = 1'd1;
-                        default: enc_idx = 1'd0;
-                    endcase
-                    4:
-                    priority if (dec_vld[0]) enc_idx = 2'd0;
-                    else     if (dec_vld[1]) enc_idx = 2'd1;
-                    else     if (dec_vld[2]) enc_idx = 2'd2;
-                    else     if (dec_vld[3]) enc_idx = 2'd3;
-                    else                     enc_idx = 2'dx;
-//                    casez (dec_vld)
-//                        4'b???1: enc_idx = 2'd0;
-//                        4'b??10: enc_idx = 2'd1;
-//                        4'b?100: enc_idx = 2'd2;
-//                        4'b1000: enc_idx = 2'd3;
-//                        default: enc_idx = 2'dx;
-//                    endcase
-//                    unique case (dec_vld) inside
-//                        4'b???1: enc_idx = 2'd0;
-//                        4'b??10: enc_idx = 2'd1;
-//                        4'b?100: enc_idx = 2'd2;
-//                        4'b1000: enc_idx = 2'd3;
-//                        default: enc_idx = 2'dx;
-//                    endcase
-//                    priority case (dec_vld) inside
-//                        4'b???1: enc_idx = 2'd0;
-//                        4'b??1?: enc_idx = 2'd1;
-//                        4'b?1??: enc_idx = 2'd2;
-//                        4'b1???: enc_idx = 2'd3;
-//                        default: enc_idx = 2'dx;
-//                    endcase
+        always_comb
+        begin
+            case (SPLIT)
+                2:
+                unique case (dec_vld) inside
+                    2'b?1  : enc_idx = 1'd0;
+                    2'b10  : enc_idx = 1'd1;
+                    default: enc_idx = 1'd0;
                 endcase
-                enc_vld = |dec_vld;
-            end
 
-
-            1:  // ADDER
-            always_comb
-            begin
-                logic [SPLIT-1:0] oht_vld;
-                logic [SPLIT-1:0] neg_vld;
-                {enc_vld, neg_vld} = $signed(-dec_vld);
-                oht_vld = dec_vld & neg_vld;
-                case (SPLIT)
-                    2:
-                    case (dec_vld)
-                        2'b01  : enc_idx = 1'd0;
-                        2'b10  : enc_idx = 1'd1;
-                        default: enc_idx = 1'dx;
-                    endcase
-                    4:
-                    case (dec_vld)
-                        4'b0001: enc_idx = 2'd0;
-                        4'b0010: enc_idx = 2'd1;
-                        4'b0100: enc_idx = 2'd2;
+                4: case (IMPLEMENTATION)
+                    0:  // casez
+                    casez (dec_vld)
+                        4'b???1: enc_idx = 2'd0;
+                        4'b??10: enc_idx = 2'd1;
+                        4'b?100: enc_idx = 2'd2;
                         4'b1000: enc_idx = 2'd3;
                         default: enc_idx = 2'dx;
                     endcase
+                    1:  // unique   if
+                    unique   if (dec_vld ==? 4'b???1) enc_idx = 2'd0;
+                    else     if (dec_vld ==? 4'b??10) enc_idx = 2'd1;
+                    else     if (dec_vld ==? 4'b?100) enc_idx = 2'd2;
+                    else     if (dec_vld ==? 4'b1000) enc_idx = 2'd3;
+                    else                              enc_idx = 2'dx;
+                    2:  // priority if
+                    priority if (dec_vld ==? 4'b???1) enc_idx = 2'd0;
+                    else     if (dec_vld ==? 4'b??1?) enc_idx = 2'd1;
+                    else     if (dec_vld ==? 4'b?1??) enc_idx = 2'd2;
+                    else     if (dec_vld ==? 4'b1???) enc_idx = 2'd3;
+                    else                              enc_idx = 2'dx;
+                    3:  // unique   case inside
+                    unique case (dec_vld) inside
+                        4'b???1: enc_idx = 2'd0;
+                        4'b??10: enc_idx = 2'd1;
+                        4'b?100: enc_idx = 2'd2;
+                        4'b1000: enc_idx = 2'd3;
+                        default: enc_idx = 2'dx;
+                    endcase
+                    4:  // priority case inside
+                    priority case (dec_vld) inside
+                        4'b???1: enc_idx = 2'd0;
+                        4'b??1?: enc_idx = 2'd1;
+                        4'b?1??: enc_idx = 2'd2;
+                        4'b1???: enc_idx = 2'd3;
+                        default: enc_idx = 2'dx;
+                    endcase
                 endcase
-            end
-
-        endcase
+            endcase
+            enc_vld = |dec_vld;
+        end
 
     end: leaf
     else begin: branch
@@ -97,7 +84,8 @@ module priority_encoder #(
         // sub-branches
         priority_encoder #(
             .WIDTH (WIDTH/SPLIT),
-            .SPLIT (SPLIT)
+            .SPLIT (SPLIT),
+            .IMPLEMENTATION (IMPLEMENTATION)
         ) encoder_sub [SPLIT-1:0] (
             .dec_vld (dec_vld),
             .enc_idx (sub_idx),
@@ -107,7 +95,8 @@ module priority_encoder #(
         // branch
         priority_encoder #(
             .WIDTH (SPLIT),
-            .SPLIT (SPLIT)
+            .SPLIT (SPLIT),
+            .IMPLEMENTATION (IMPLEMENTATION)
         ) encoder_brn (
             .dec_vld (sub_vld),
             .enc_idx (brn_idx),

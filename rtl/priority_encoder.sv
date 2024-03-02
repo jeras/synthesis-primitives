be54a94da8493c20b3810e4d50e67ad747821343
Iztok Jeras <iztok.jeras@gmail.com>
2024-03-02 14:57:05 +0100

    tried various styles of writing the 4->1 base encoder, found Vivado simulator issue

--- rtl/priority_encoder.sv
+++ rtl/priority_encoder.sv
@@ -6,74 +6,118 @@ module priority_encoder #(
     localparam int unsigned WIDTH_LOG = $clog2(WIDTH),
     localparam int unsigned SPLIT_LOG = $clog2(SPLIT),
     // implementation
-    bit mode = 1'b1
+    parameter  int unsigned MODE = 0  // supported modes are 0-LUT, 1-ADDER and 2-FUNCTION
 )(
     input  logic [SPLIT-1:0][WIDTH/SPLIT-1:0] dec_vld,
     output logic            [WIDTH_LOG  -1:0] enc_idx,
     output logic                              enc_vld
 );
 
-    function logic [SPLIT_LOG-1:0] encode (logic [SPLIT-1:0] valid);
-        case (SPLIT)
-            2:  case (valid)
-                    2'bx1  : encode = 1'd0;
-                    2'b10  : encode = 1'd1;
-                    default: encode = 1'd0;
-                endcase
-            4:  case (valid)
-                    4'bxxx1: encode = 2'd0;
-                    4'bxx10: encode = 2'd1;
-                    4'bx100: encode = 2'd2;
-                    4'b1000: encode = 2'd3;
-                    default: encode = 2'd0;
-                endcase
-        endcase
-    endfunction: encode
-
     generate
-    if (mode == 1'b0) begin: simple
+    if (WIDTH == SPLIT) begin: leaf
 
-    simple_encoder #(
-        .WIDTH (WIDTH)
-    ) simple_encoder (
-        .dec_vld (dec_vld & (-dec_vld)),
-        .enc_idx (enc_idx),
-        .enc_vld (enc_vld)
-    );
+        case (MODE)
 
-    end: simple
-    else begin: recursive
+            0:  // LUT
+            always_comb
+            begin
+                case (SPLIT)
+                    2:
+                    unique case (dec_vld) inside
+                        2'b?1  : enc_idx = 1'd0;
+                        2'b10  : enc_idx = 1'd1;
+                        default: enc_idx = 1'd0;
+                    endcase
+                    4:
+                    priority if (dec_vld[0]) enc_idx = 2'd0;
+                    else     if (dec_vld[1]) enc_idx = 2'd1;
+                    else     if (dec_vld[2]) enc_idx = 2'd2;
+                    else     if (dec_vld[3]) enc_idx = 2'd3;
+                    else                     enc_idx = 2'dx;
+//                    casez (dec_vld)
+//                        4'b???1: enc_idx = 2'd0;
+//                        4'b??10: enc_idx = 2'd1;
+//                        4'b?100: enc_idx = 2'd2;
+//                        4'b1000: enc_idx = 2'd3;
+//                        default: enc_idx = 2'dx;
+//                    endcase
+//                    unique case (dec_vld) inside
+//                        4'b???1: enc_idx = 2'd0;
+//                        4'b??10: enc_idx = 2'd1;
+//                        4'b?100: enc_idx = 2'd2;
+//                        4'b1000: enc_idx = 2'd3;
+//                        default: enc_idx = 2'dx;
+//                    endcase
+//                    priority case (dec_vld) inside
+//                        4'b???1: enc_idx = 2'd0;
+//                        4'b??1?: enc_idx = 2'd1;
+//                        4'b?1??: enc_idx = 2'd2;
+//                        4'b1???: enc_idx = 2'd3;
+//                        default: enc_idx = 2'dx;
+//                    endcase
+                endcase
+                enc_vld = |dec_vld;
+            end
 
-        if (WIDTH == SPLIT) begin: leaf
 
-            // leaf
-            assign enc_idx = encode(dec_vld);
-            assign enc_vld = |dec_vld;
+            1:  // ADDER
+            always_comb
+            begin
+                logic [SPLIT-1:0] oht_vld;
+                logic [SPLIT-1:0] neg_vld;
+                {enc_vld, neg_vld} = $signed(-dec_vld);
+                oht_vld = dec_vld & neg_vld;
+                case (SPLIT)
+                    2:
+                    case (dec_vld)
+                        2'b01  : enc_idx = 1'd0;
+                        2'b10  : enc_idx = 1'd1;
+                        default: enc_idx = 1'dx;
+                    endcase
+                    4:
+                    case (dec_vld)
+                        4'b0001: enc_idx = 2'd0;
+                        4'b0010: enc_idx = 2'd1;
+                        4'b0100: enc_idx = 2'd2;
+                        4'b1000: enc_idx = 2'd3;
+                        default: enc_idx = 2'dx;
+                    endcase
+                endcase
+            end
 
-        end: leaf
-        else begin: branch
+        endcase
 
-            logic [SPLIT-1:0] [WIDTH_LOG-SPLIT_LOG-1:0] sub_idx;
-            logic [SPLIT-1:0]                           sub_vld;
-            logic                       [SPLIT_LOG-1:0] tmp_idx;
+    end: leaf
+    else begin: branch
 
-            priority_encoder #(
-               .WIDTH (WIDTH/SPLIT),
-               .SPLIT (SPLIT)
-            ) encoder [SPLIT-1:0] (
-                .dec_vld (dec_vld),
-                .enc_idx (sub_idx),
-                .enc_vld (sub_vld)
-            );
+        logic [SPLIT-1:0] [WIDTH_LOG-SPLIT_LOG-1:0] sub_idx;
+        logic [SPLIT-1:0]                           sub_vld;
+        logic                       [SPLIT_LOG-1:0] brn_idx;
 
-            // branch
-            assign tmp_idx = encode(sub_vld);
-            assign enc_idx = {tmp_idx, sub_idx[tmp_idx]};
-            assign enc_vld = |sub_vld;
+        // sub-branches
+        priority_encoder #(
+            .WIDTH (WIDTH/SPLIT),
+            .SPLIT (SPLIT)
+        ) encoder_sub [SPLIT-1:0] (
+            .dec_vld (dec_vld),
+            .enc_idx (sub_idx),
+            .enc_vld (sub_vld)
+        );
 
-        end: branch
+        // branch
+        priority_encoder #(
+            .WIDTH (SPLIT),
+            .SPLIT (SPLIT)
+        ) encoder_brn (
+            .dec_vld (sub_vld),
+            .enc_idx (brn_idx),
+            .enc_vld (enc_vld)
+        );
 
-    end: recursive
+        // multiplex sub-branches into branch
+        assign enc_idx = {brn_idx, sub_idx[brn_idx]};
+
+    end: branch
     endgenerate
 
 endmodule: priority_encoder

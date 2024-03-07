115c27c4363aa051b47e626c515c2500a59e5e25
Iztok Jeras <iztok.jeras@gmail.com>
2024-03-07 23:49:06 +0100

    first attempt at decoder with congigurable priority

--- encoder_tb.sv
+++ encoder_tb.sv
@@ -1,7 +1,7 @@
 module encoder_tb #(
     // size parameters
-    int unsigned WIDTH = 16,
-    int unsigned SPLIT = 4
+    int unsigned WIDTH = 4,
+    int unsigned SPLIT = 2
 );
 
     // size local parameters
@@ -16,17 +16,24 @@ module encoder_tb #(
     // input
     logic [WIDTH    -1:0] dec_vld;
     // priority encoder
+    logic [WIDTH_LOG-1:0] enc_pri;
     logic [WIDTH_LOG-1:0] enc_idx [0:IMPLEMENTATIONS-1];
     logic                 enc_vld [0:IMPLEMENTATIONS-1];
     // reference encoder
     logic [WIDTH_LOG-1:0] ref_enc_idx;
     logic                 ref_enc_vld;
 
+    logic                 wrong   [0:IMPLEMENTATIONS-1];
+    string                test;
+
     function  [WIDTH_LOG-1:0] encoder (
-        logic [WIDTH    -1:0] dec_vld
+        logic [WIDTH    -1:0] dec_vld,
+        logic [WIDTH_LOG-1:0] enc_pri
     );
         for (int unsigned i=0; i<WIDTH; i++) begin
-            if (dec_vld[i] == 1'b1)  return WIDTH_LOG'(i);
+            int unsigned t;
+            t = (i + enc_pri) % WIDTH;
+            if (dec_vld[t] === 1'b1)  return WIDTH_LOG'(t);
         end
         return 'x;
     endfunction: encoder
@@ -34,16 +41,16 @@ module encoder_tb #(
     // reference encoder
     always_comb
     begin
-        ref_enc_idx = encoder(dec_vld);
+        ref_enc_idx = encoder(dec_vld, enc_pri);
         ref_enc_vld =       |(dec_vld);    
     end
 
     // output checking task
     task check();
         for (int unsigned i=0; i<IMPLEMENTATIONS; i++) begin
-            assert (enc_vld[i] == ref_enc_vld) else $error("IMPLEMENTATION[%d]:  enc_vld != 1'b%b" , i,            ref_enc_vld);
+            assert (enc_vld[i] == ref_enc_vld) else $error("@%t IMPLEMENTATION[%d]:  enc_vld != 1'b%b" , $time, i,            ref_enc_vld);
             if (enc_vld[i]) begin  // do not check the encoded output, if it is not supposed to be valid
-            assert (enc_idx[i] == ref_enc_idx) else $error("IMPLEMENTATION[%d]:  enc_idx != %d'd%d", i, WIDTH_LOG, ref_enc_idx);
+            assert (enc_idx[i] == ref_enc_idx) else $error("@%t IMPLEMENTATION[%d]:  enc_idx != %d'd%d", $time, i, WIDTH_LOG, ref_enc_idx);
             end
         end
     endtask: check
@@ -52,61 +59,75 @@ module encoder_tb #(
     initial
     begin
         // idle test
+        test = "zero";
         dec_vld <= '0;
+        enc_pri <= WIDTH_LOG'(0);
         #T;
         check;
         #T;
+        for (int unsigned pri=0; pri<WIDTH; pri++) begin: for_pri
+            enc_pri <= WIDTH_LOG'(pri);
 
-        // one-hot encoder test
-        for (int unsigned i=0; i<WIDTH; i++) begin
-            logic [WIDTH-1:0] tmp_vld;
-            tmp_vld = '0;
-            tmp_vld[i] = 1'b1;
-            dec_vld <= tmp_vld;
-            #T;
-            check;
-            #T;
-        end
+            // one-hot encoder test
+            test = "one-hot";
+            for (int unsigned i=0; i<WIDTH; i++) begin: for_oht
+                logic [WIDTH-1:0] tmp_vld;
+                tmp_vld = '0;
+                tmp_vld[i] = 1'b1;
+                dec_vld = tmp_vld;
+                #T;
+                check;
+                #T;
+            end: for_oht
 
-        // priority encoder test (with undefined inputs)
-        for (int unsigned i=0; i<WIDTH; i++) begin
-            logic [WIDTH-1:0] tmp_vld;
-            tmp_vld = 'X;
-            for (int unsigned j=0; j<i; j++) begin
-                tmp_vld[j] = 1'b0;
-            end
-            tmp_vld[i] = 1'b1;
-            dec_vld <= tmp_vld;
-            #T;
-            check;
-            #T;
-        end
-        $finish;
+            // priority encoder test (with undefined inputs)
+            test = "priority";
+            for (int unsigned i=0; i<WIDTH; i++) begin: for_pri
+                logic [WIDTH-1:0] tmp_vld;
+                for (int unsigned j=0; j<WIDTH; j++) begin
+                    int unsigned t;
+                    t = (j + pri) % WIDTH;
+                         if (j< i)  tmp_vld[t] = 1'b0;
+                    else if (j==i)  tmp_vld[t] = 1'b1;
+                    else            tmp_vld[t] = 1'bx;
+                end
+                dec_vld <= tmp_vld;
+                #T;
+                check;
+                #T;
+            end: for_pri
 
-        // priority encoder test (going through all input combinations)
-        for (logic unsigned [WIDTH-1:0] tmp_vld='1; tmp_vld>0; tmp_vld--) begin
-            dec_vld <= {<<{tmp_vld}};
-            #T;
-            check;
-            #T;
-        end
+            // priority encoder test (going through all input combinations)
+            test = "all";
+            for (logic unsigned [WIDTH-1:0] tmp_vld='1; tmp_vld!=0; tmp_vld--) begin: for_all
+                dec_vld <= {<<{tmp_vld}};
+                #T;
+                check;
+                #T;
+            end: for_all
+        end: for_pri
         $finish;
     end
 
     generate
-    for (genvar i=0; i<IMPLEMENTATIONS; i++) begin
+    for (genvar i=0; i<IMPLEMENTATIONS; i++) begin: gen_imp
 
+        // output checking
+        assign wrong [i] = enc_idx[i] != ref_enc_idx;
+
+        // DUT RTL instance
         priority_encoder #(
             .WIDTH (WIDTH),
             .SPLIT (SPLIT),
             .IMPLEMENTATION (i)
         ) priority_encoder__casez (
             .dec_vld (dec_vld),
+            .enc_pri (enc_pri),
             .enc_idx (enc_idx[i]),
             .enc_vld (enc_vld[i])
         );
 
-    end
+    end: gen_imp
     endgenerate
 
 endmodule: encoder_tb
--- priority_encoder.sv
+++ priority_encoder.sv
@@ -15,6 +15,7 @@ module priority_encoder #(
 )(
 //    input  logic [SPLIT-1:0][WIDTH/SPLIT-1:0] dec_vld,
     input  logic            [WIDTH      -1:0] dec_vld,
+    input  logic            [WIDTH_LOG  -1:0] enc_pri,
     output logic            [WIDTH_LOG  -1:0] enc_idx,
     output logic                              enc_vld
 );
@@ -22,64 +23,80 @@ module priority_encoder #(
     generate
     if (WIDTH == SPLIT) begin: leaf
 
+        logic [SPLIT    -1:0] tmp_vld;
+        logic [SPLIT_LOG-1:0] tmp_idx;
+
+        // reorder decoded valid array for desired priority
+        for (genvar i=0; i<SPLIT; i++) begin: reorder
+            assign tmp_vld[i] = dec_vld[i[SPLIT_LOG-1:0] ^ enc_pri];
+        end: reorder
+
         always_comb
         begin
             case (SPLIT)
                 2:
-                unique case (dec_vld) inside
-                    2'b?1  : enc_idx = 1'd0;
-                    2'b10  : enc_idx = 1'd1;
-                    default: enc_idx = 1'd0;
+                unique case (tmp_vld) inside
+                    2'b?1  : tmp_idx = 1'd0;
+                    2'b10  : tmp_idx = 1'd1;
+                    default: tmp_idx = 1'd0;
                 endcase
 
                 4: case (IMPLEMENTATION)
                     0:  // casez
-                    casez (dec_vld)
-                        4'b???1: enc_idx = 2'd0;
-                        4'b??10: enc_idx = 2'd1;
-                        4'b?100: enc_idx = 2'd2;
-                        4'b1000: enc_idx = 2'd3;
-                        default: enc_idx = 2'dx;
+                    casez (tmp_vld)
+                        4'b???1: tmp_idx = 2'd0;
+                        4'b??10: tmp_idx = 2'd1;
+                        4'b?100: tmp_idx = 2'd2;
+                        4'b1000: tmp_idx = 2'd3;
+                        default: tmp_idx = 2'dx;
                     endcase
                     1:  // unique   if
-                    unique   if (dec_vld ==? 4'b???1) enc_idx = 2'd0;
-                    else     if (dec_vld ==? 4'b??10) enc_idx = 2'd1;
-                    else     if (dec_vld ==? 4'b?100) enc_idx = 2'd2;
-                    else     if (dec_vld ==? 4'b1000) enc_idx = 2'd3;
-                    else                              enc_idx = 2'dx;
+                    unique   if (tmp_vld ==? 4'b???1) tmp_idx = 2'd0;
+                    else     if (tmp_vld ==? 4'b??10) tmp_idx = 2'd1;
+                    else     if (tmp_vld ==? 4'b?100) tmp_idx = 2'd2;
+                    else     if (tmp_vld ==? 4'b1000) tmp_idx = 2'd3;
+                    else                              tmp_idx = 2'dx;
                     2:  // priority if
-                    priority if (dec_vld ==? 4'b???1) enc_idx = 2'd0;
-                    else     if (dec_vld ==? 4'b??1?) enc_idx = 2'd1;
-                    else     if (dec_vld ==? 4'b?1??) enc_idx = 2'd2;
-                    else     if (dec_vld ==? 4'b1???) enc_idx = 2'd3;
-                    else                              enc_idx = 2'dx;
+                    priority if (tmp_vld ==? 4'b???1) tmp_idx = 2'd0;
+                    else     if (tmp_vld ==? 4'b??1?) tmp_idx = 2'd1;
+                    else     if (tmp_vld ==? 4'b?1??) tmp_idx = 2'd2;
+                    else     if (tmp_vld ==? 4'b1???) tmp_idx = 2'd3;
+                    else                              tmp_idx = 2'dx;
                     3:  // unique   case inside
-                    unique case (dec_vld) inside
-                        4'b???1: enc_idx = 2'd0;
-                        4'b??10: enc_idx = 2'd1;
-                        4'b?100: enc_idx = 2'd2;
-                        4'b1000: enc_idx = 2'd3;
-                        default: enc_idx = 2'dx;
+                    unique case (tmp_vld) inside
+                        4'b???1: tmp_idx = 2'd0;
+                        4'b??10: tmp_idx = 2'd1;
+                        4'b?100: tmp_idx = 2'd2;
+                        4'b1000: tmp_idx = 2'd3;
+                        default: tmp_idx = 2'dx;
                     endcase
                     4:  // priority case inside
-                    priority case (dec_vld) inside
-                        4'b???1: enc_idx = 2'd0;
-                        4'b??1?: enc_idx = 2'd1;
-                        4'b?1??: enc_idx = 2'd2;
-                        4'b1???: enc_idx = 2'd3;
-                        default: enc_idx = 2'dx;
+                    priority case (tmp_vld) inside
+                        4'b???1: tmp_idx = 2'd0;
+                        4'b??1?: tmp_idx = 2'd1;
+                        4'b?1??: tmp_idx = 2'd2;
+                        4'b1???: tmp_idx = 2'd3;
+                        default: tmp_idx = 2'dx;
                     endcase
                 endcase
             endcase
             enc_vld = |dec_vld;
         end
 
+        // reorder
+        assign enc_idx = tmp_idx ^ enc_pri;
+
     end: leaf
     else begin: branch
 
+        logic             [WIDTH_LOG-SPLIT_LOG-1:0] sub_pri;
         logic [SPLIT-1:0] [WIDTH_LOG-SPLIT_LOG-1:0] sub_idx;
         logic [SPLIT-1:0]                           sub_vld;
+        logic                       [SPLIT_LOG-1:0] brn_pri;
         logic                       [SPLIT_LOG-1:0] brn_idx;
+
+        // input priorities
+        assign {brn_pri, sub_pri} = enc_pri;
 
         // sub-branches
         priority_encoder #(
@@ -88,6 +105,7 @@ module priority_encoder #(
             .IMPLEMENTATION (IMPLEMENTATION)
         ) encoder_sub [SPLIT-1:0] (
             .dec_vld (dec_vld),
+            .enc_pri (sub_pri),
             .enc_idx (sub_idx),
             .enc_vld (sub_vld)
         );
@@ -99,6 +117,7 @@ module priority_encoder #(
             .IMPLEMENTATION (IMPLEMENTATION)
         ) encoder_brn (
             .dec_vld (sub_vld),
+            .enc_pri (brn_pri),
             .enc_idx (brn_idx),
             .enc_vld (enc_vld)
         );

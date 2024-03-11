8bd580280a5aa8f1643a6449021c79e30197c15e
Iztok Jeras <iztok.jeras@gmail.com>
2024-03-11 20:21:18 +0100

    PPE: programmable priority encoder is working properly now

--- priority_encoder.sv
+++ priority_encoder.sv
@@ -19,6 +19,8 @@ module priority_encoder #(
     input  logic            [WIDTH_LOG  -1:0] enc_pri,
     output logic            [WIDTH_LOG  -1:0] enc_idx_h,
     output logic            [WIDTH_LOG  -1:0] enc_idx_l,
+    output logic                              enc_sel_h,
+    output logic                              enc_sel_l,
     output logic                              enc_vld_h,
     output logic                              enc_vld_l
 );
@@ -34,21 +36,21 @@ module priority_encoder #(
                     1'b0:
                     begin
                         unique case ({dec_vld_h[1], dec_vld_l[1], dec_vld_h[0]}) inside
-                            3'b??_1: begin enc_idx_h = 1'd0; enc_vld_h = 1'd1; end
-                            3'b?1_0: begin enc_idx_h = 1'd1; enc_vld_h = 1'd1; end
-                            3'b10_0: begin enc_idx_h = 1'd1; enc_vld_h = 1'd1; end
-                            default: begin enc_idx_h = 1'dx; enc_vld_h = 1'd0; end
+                            3'b??_1: begin enc_idx_h = 1'd0; enc_sel_h = 1'b1; enc_vld_h = 1'd1; end
+                            3'b?1_0: begin enc_idx_h = 1'd1; enc_sel_h = 1'b0; enc_vld_h = 1'd1; end
+                            3'b10_0: begin enc_idx_h = 1'd1; enc_sel_h = 1'b1; enc_vld_h = 1'd1; end
+                            default: begin enc_idx_h = 1'dx; enc_sel_h = 1'bx; enc_vld_h = 1'd0; end
                         endcase
-                                     begin enc_idx_l = 1'd0; enc_vld_l = dec_vld_l[0]; end
+                                     begin enc_idx_l = 1'd0; enc_sel_l = 1'b0; enc_vld_l = dec_vld_l[0]; end
                     end
                     1'b1:
                     begin
-                                     begin enc_idx_h = 1'd1; enc_vld_h = dec_vld_h[1]; end
+                                     begin enc_idx_h = 1'd1; enc_sel_h = 1'b1; enc_vld_h = dec_vld_h[1]; end
                         unique case ({dec_vld_l[1], dec_vld_h[0], dec_vld_l[0]}) inside
-                            3'b?_?1: begin enc_idx_l = 1'd0; enc_vld_l = 1'd1; end
-                            3'b?_1?: begin enc_idx_l = 1'd0; enc_vld_l = 1'd1; end
-                            3'b1_0?: begin enc_idx_l = 1'd1; enc_vld_l = 1'd1; end
-                            default: begin enc_idx_l = 1'dx; enc_vld_l = 1'd0; end
+                            3'b?_?1: begin enc_idx_l = 1'd0; enc_sel_l = 1'd0; enc_vld_l = 1'd1; end
+                            3'b?_10: begin enc_idx_l = 1'd0; enc_sel_l = 1'd1; enc_vld_l = 1'd1; end
+                            3'b1_00: begin enc_idx_l = 1'd1; enc_sel_l = 1'd0; enc_vld_l = 1'd1; end
+                            default: begin enc_idx_l = 1'dx; enc_sel_l = 1'dx; enc_vld_l = 1'd0; end
                         endcase
                     end
                 endcase
@@ -66,6 +68,8 @@ module priority_encoder #(
         logic                       [SPLIT_LOG-1:0] brn_pri;
         logic                       [SPLIT_LOG-1:0] brn_idx_h;
         logic                       [SPLIT_LOG-1:0] brn_idx_l;
+        logic                                       brn_sel_h;
+        logic                                       brn_sel_l;
 
         // input priorities
         assign {brn_pri, sub_pri} = enc_pri;
@@ -81,6 +85,8 @@ module priority_encoder #(
             .enc_pri   (sub_pri  ),
             .enc_idx_h (sub_idx_h),
             .enc_idx_l (sub_idx_l),
+            .enc_sel_h (),
+            .enc_sel_l (),
             .enc_vld_h (sub_vld_h),
             .enc_vld_l (sub_vld_l)
         );
@@ -96,13 +102,15 @@ module priority_encoder #(
             .enc_pri   (brn_pri  ),
             .enc_idx_h (brn_idx_h),
             .enc_idx_l (brn_idx_l),
+            .enc_sel_h (brn_sel_h),
+            .enc_sel_l (brn_sel_l),
             .enc_vld_h (enc_vld_h),
             .enc_vld_l (enc_vld_l)
         );
 
         // multiplex sub-branches into branch
-        assign enc_idx_h = {brn_idx_h, sub_idx_h[brn_idx_h]};
-        assign enc_idx_l = {brn_idx_l, sub_idx_l[brn_idx_l]};
+        assign enc_idx_h = {brn_idx_h, brn_sel_h ? sub_idx_h[brn_idx_h] : sub_idx_l[brn_idx_h]};
+        assign enc_idx_l = {brn_idx_l, brn_sel_l ? sub_idx_h[brn_idx_l] : sub_idx_l[brn_idx_l]};
 
     end: branch
     endgenerate

e30671cfe8f8408632d195f205a75e032cbdc73f
Iztok Jeras <iztok.jeras@gmail.com>
2024-03-31 15:43:44 +0200

    added equivalence_comparator and license

---
+++ rtl/equivalence_comparator.sv
@@ -0,0 +1,20 @@
+///////////////////////////////////////////////////////////////////////////////
+// Equivalence comparator
+//
+// @author: Iztok Jeras <iztok.jeras@gmail.com>
+//
+// Licensed under CERN-OHL-P v2 or later
+///////////////////////////////////////////////////////////////////////////////
+
+module magnitude_comparator_base #(
+    // size parameters
+    parameter  int unsigned WIDTH = 32
+)(
+    input  logic [WIDTH-1:0] i_a,
+    input  logic [WIDTH-1:0] i_b,
+    output logic             eq
+);
+
+    assign eq = i_a == i_b;
+
+endmodule: magnitude_comparator_base

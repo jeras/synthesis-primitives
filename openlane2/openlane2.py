83e762222cc4a0ee8e30b5deac26debc2f6cc332
Iztok Jeras <iztok.jeras@gmail.com>
2024-06-25 18:56:25 +0200

    cretated iteration over all parameter combinations

--- openlane2/openlane2.py
+++ openlane2/openlane2.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 
-import openlane
 import os
 
+import itertools
+
+import openlane
 from openlane.flows import SequentialFlow
 from openlane.steps import Yosys, Misc, OpenROAD, Magic, Netgen
 
@@ -27,25 +29,48 @@ class MyFlow(SequentialFlow):
 
 print(openlane.__version__)
 
-tops=[
-    "eql_cmp",
-    "mux_bin_base",
-    "mux_oht_base"
+designs=[
+    {'top': "bin2oht_base", 'parameters': {"IMPLEMENTATION": [0, 1, 2, 3], "WIDTH": [4,6,8,12,16,24,32,48,64]}},
+#    "oht2bin_base",
+#    "eql_cmp",
+#    "mag_cmp_base",
+#    "mux_bin_base",
+#    "mux_oht_base",
 ]
 
-for top in tops:
-    flow = MyFlow(
-        {
-            "PDK": "sky130A",
-            "DESIGN_NAME": top,
-            "USE_SYNLIG": True,
-            "VERILOG_FILES": [
-                "../rtl/eql_cmp.sv",
-                "../rtl/mux_bin_base.sv",
-                "../rtl/mux_oht_base.sv"
-            ],
-            "CLOCK_PORT": None,
-        },
-        design_dir=".",
-    )
-    flow.start(tag=top)
+for design in designs:
+    top = design['top']
+    # iterate over all parameter combinations
+    combinations = [dict(zip(design['parameters'], x)) for x in itertools.product(*design['parameters'].values())]
+    for combination in combinations:
+        parameters = [f"{parameter}={value}" for parameter, value in combination.items()]
+        combination_name = '_'.join([f"{parameter}_{value}" for parameter, value in combination.items()])
+        flow = MyFlow(
+            {
+                "PDK": "sky130A",
+                "DESIGN_NAME": top,
+                "USE_SYNLIG": True,
+                "SYNTH_PARAMETERS": parameters,
+                "VERILOG_FILES": [
+                    "../rtl/bin2oht_base.sv",
+                    "../rtl/bin2oht_tree.sv",
+                    "../rtl/bin2oht.sv",
+                    "../rtl/oht2bin_base.sv",
+                    "../rtl/oht2bin_tree.sv",
+                    "../rtl/oht2bin.sv",
+                    "../rtl/eql_cmp.sv",
+                    "../rtl/mag_cmp_base.sv",
+                    "../rtl/mag_cmp_tree.sv",
+                    "../rtl/mag_cmp.sv",
+                    "../rtl/mux_bin_base.sv",
+                    "../rtl/mux_bin_tree.sv",
+                    "../rtl/mux_bin.sv",
+                    "../rtl/mux_oht_base.sv",
+                    "../rtl/mux_oht_tree.sv",
+                    "../rtl/mux_oht.sv",
+                ],
+                "CLOCK_PORT": None,
+            },
+            design_dir=".",
+        )
+        flow.start(tag=top+combination_name)

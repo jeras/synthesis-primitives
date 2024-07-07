1c3fa4ae8d477447f290f1c6c96f1d83d12d89f4
Iztok Jeras <iztok.jeras@gmail.com>
2024-07-07 21:26:33 +0200

    update to openlane synthesis script, with focus on netlistsvg

--- openlane2/openlane2.py
+++ openlane2/openlane2.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import os
+import subprocess
 import itertools
 from jinja2 import Environment, FileSystemLoader
 
@@ -36,11 +37,12 @@ designs=[
 #   {'top': "bin2oht_base", 'parameters': {"IMPLEMENTATION": [0, 1, 2, 3], "WIDTH": width_range}},
 #   {'top': "oht2bin_base", 'parameters': {"IMPLEMENTATION": [0, 1], "DIRECTION": ['LSB', 'MSB'], "WIDTH": width_range}},
 #   {'top': "pry2oht_base", 'parameters': {"IMPLEMENTATION": [0, 1, 2], "DIRECTION": ['LSB', 'MSB'], "WIDTH": width_range}},
-#   {'top': "eql_cmp", 'parameters': {"IMPLEMENTATION": [0, 1, 2, 3, 4], "WIDTH": width_range}},
-   {'top': "mag_cmp_base", 'parameters': {"IMPLEMENTATION": [0], "WIDTH": width_range}},
-#   {'top': "mux_bin_base", 'parameters': {"IMPLEMENTATION": [0], "WIDTH": width_range}},
-#   {'top': "mux_oht_base", 'parameters': {"IMPLEMENTATION": [0, 1], "WIDTH": width_range}},
-    {'top': "add_base"    , 'parameters': {"IMPLEMENTATION": [0], "WIDTH": width_range}},
+#   {'top': "eql_cmp"    , 'parameters': {"IMPLEMENTATION": [0, 1, 2, 3, 4], "WIDTH": width_range}},
+#   {'top': "mag_cmp_base", 'parameters': {"IMPLEMENTATION": [0]   , "WIDTH": width_range}},
+#   {'top': "mux_bin_base", 'parameters': {"IMPLEMENTATION": [0]   , "WIDTH": width_range}},
+#   {'top': "mux_pry_base", 'parameters': {"IMPLEMENTATION": [0]   , "WIDTH": width_range}},
+    {'top': "mux_oht"     , 'parameters': {"IMPLEMENTATION": [0, 1], "WIDTH": width_range}},
+#   {'top': "add_base"    , 'parameters': {"IMPLEMENTATION": [0]   , "WIDTH": width_range}},
 ]
 
 report_environment = Environment(loader=FileSystemLoader("."))
@@ -80,9 +82,10 @@ for design in designs:
                     "../rtl/mux_bin_base.sv",
                     "../rtl/mux_bin_tree.sv",
                     "../rtl/mux_bin.sv",
-                    "../rtl/mux_oht_base.sv",
-                    "../rtl/mux_oht_tree.sv",
                     "../rtl/mux_oht.sv",
+                    "../rtl/mux_pry_base.sv",
+                    "../rtl/mux_pry_tree.sv",
+                    "../rtl/mux_pry.sv",
                     "../rtl/add_base.sv",
                 ],
                 "CLOCK_PORT": None,
@@ -93,9 +96,15 @@ for design in designs:
 
         step_yosys = next(step for step in flow.step_objects if isinstance(step, Yosys.Synthesis))
         step_yosys_dir = os.path.relpath(step_yosys.step_dir, os.getcwd())
+        # convert JSON netlist into a SVG diagram
+        subprocess.run(["netlistsvg", step_yosys_dir+"/proc.json"             , "-o", step_yosys_dir+"/proc.svg"             ])
+        subprocess.run(["netlistsvg", step_yosys_dir+"/primitive_techmap.json", "-o", step_yosys_dir+"/primitive_techmap.svg"])
+        subprocess.run(["netlistsvg", step_yosys_dir+"/"+top+".nl.v.json"     , "-o", step_yosys_dir+"/netlist.svg"          ])
+        # populate context
         design_context['combinations'].append({
             'parameters': list(combination.values()),
             'hierarchy'        : "[SVG]("+step_yosys_dir+"/hierarchy.svg)",
+            'proc'             : "[SVG]("+step_yosys_dir+"/proc.svg)",
             'primitive_techmap': "[SVG]("+step_yosys_dir+"/primitive_techmap.svg)",
             'netlist'          : "[SVG]("+step_yosys_dir+"/netlist.svg)",
         })

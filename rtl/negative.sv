58299f4ce3f26033ab1cf383948f9211642d7451
Iztok Jeras <iztok.jeras@gmail.com>
2024-07-28 04:45:50 +0200

    removing clock/reset from rtl/negative.sv

--- rtl/negative.sv
+++ rtl/negative.sv
@@ -1,25 +1,11 @@
 module negative #(
     int unsigned WIDTH = 32
 )(
-    // system signals
-    input  logic clk,
-    input  logic rst,
     // data signals
     input  logic [WIDTH-1:0] xi,
     output logic [WIDTH-1:0] xo
 );
 
-    // input register
-    logic [WIDTH-1:0] xr;
-
-    // use synchronous reset
-    always @(posedge clk)
-    if (rst)  xr <= '0;
-    else      xr <= xi;
-
-    // use synchronous reset
-    always @(posedge clk)
-    if (rst)  xo <= '0;
-    else      xo <= -xr;
+    assign xo = -xi;
 
 endmodule: negative

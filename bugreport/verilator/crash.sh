16a3a6c8bc1060e12dbe1cb92472ed68e587ba88
Iztok Jeras <iztok.jeras@gmail.com>
2024-10-07 13:20:30 +0200

    added verilator crach report

---
+++ bugreport/verilator/crash.sh
@@ -0,0 +1,1 @@
+verilator --binary --timing --top crash -Wno-INITIALDLY -Wno-UNOPTFLAT --trace-fst crash.sv
---
+++ bugreport/verilator/crash.sv
@@ -0,0 +1,5 @@
+module crash ();
+
+    localparam logic [4-1:0] max_lst [5] = '{0, 1, 2**4-1};
+
+endmodule: crash

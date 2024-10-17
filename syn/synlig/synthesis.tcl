d29837c402a08a7c0a78c9f2eb69ec5133ddbb47
Iztok Jeras <iztok.jeras@gmail.com>
2024-10-17 13:45:02 +0200

    generalize script

--- syn/synlig/synthesis.tcl
+++ syn/synlig/synthesis.tcl
@@ -39,8 +39,8 @@ techmap; opt
 debug_show "techmap"
 
 # mapping logic to mycells.lib
-dfflibmap -liberty /home/izi/.volare/sky130A/libs.ref/sky130_fd_sc_hd/lib/sky130_fd_sc_hd__tt_025C_1v80.lib
-abc -liberty /home/izi/.volare/sky130A/libs.ref/sky130_fd_sc_hd/lib/sky130_fd_sc_hd__tt_025C_1v80.lib
+dfflibmap -liberty $::env(HOME)/.volare/sky130A/libs.ref/sky130_fd_sc_hd/lib/sky130_fd_sc_hd__tt_025C_1v80.lib
+abc -liberty $::env(HOME)/.volare/sky130A/libs.ref/sky130_fd_sc_hd/lib/sky130_fd_sc_hd__tt_025C_1v80.lib
 clean
 
 debug_show "map"
